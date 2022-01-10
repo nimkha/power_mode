a7c9b82d91ebf0e4f777671bd1566a222b38fc02
nimkha <nimak47@gmail.com>
2022-01-10 21:07:41 +0100

    fixed script

--- set_power_mode.py
+++ set_power_mode.py
@@ -4,7 +4,6 @@ import subprocess
 
 power_mode = subprocess.run(["cat", "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"], capture_output=True, encoding="utf8")
 
-print(power_mode.stdout)
-
 if power_mode.stdout.strip() != "performance":
-    subprocess.run(["echo", "performance", "|", "sudo", "tee", "/sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"])
+    subprocess.run("echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor", shell=True)
+
